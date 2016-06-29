01ca6a85e2d0923bc2fa786b62754a85aa60db6b
XronisK <chkoutsios@yahoo.com>
2016-06-29 21:08:57 +0300

    added a test avr gcc null project

---
+++ prj_testGit/prj_testGit.c
@@ -0,0 +1,17 @@
+/*
+ * prj_testGit.c
+ *
+ * Created: 6/29/2016 8:49:29 PM
+ *  Author: chk
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
